26d08d43f91b5ce6039fabd56c4e1738c0f19fe7
Amar Daxini <amardaxini@gmail.com>
2013-11-14 13:20:44 +0530

    added after sign in for merchantand updated sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   layout :set_layout
 
+
+  def after_sign_in_path_for(resource)
+    # IF it's merchant redirect to merchant page
+    if resource.is_a?(User)
+      merchant_merchants_path
+    end
+  end
   private
   
 
