0b1010ffa1e3c8606bbaf69290a7b5d6fbf6e82c
Gayatri Dhatrak <dhatrak.gayatri@gmail.com>
2013-11-22 18:45:06 +0530

    Removed unwanted models

--- app/models/outlet_ad.rb
+++ app/models/outlet_ad.rb
@@ -1,7 +1,5 @@
 class OutletAd < ActiveRecord::Base
   attr_accessible :ad_id, :is_deleted, :outlet_id
-  has_many :multiple_codes_specific_outlets, :dependent => :destroy
-  has_many :single_code_specific_outlets, :dependent => :destroy
   belongs_to :ad
   belongs_to :outlet
 end
