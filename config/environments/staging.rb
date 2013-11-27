9bf47e6ae4d2cd470932f76e897ac5728810c7f7
Amar Daxini <amardaxini@gmail.com>
2013-11-27 11:01:56 +0530

    added email support for staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -60,6 +60,17 @@ Gullak2::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = { host: 'staging.gullakmaster.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :user_name => 'amargullak@gmail.com',
+  :password => 'l5yx2LwiNEOb95oGulzRQQ',
+  :domain => 'yourdomain.com',
+  :address => 'smtp.mandrillapp.com',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
