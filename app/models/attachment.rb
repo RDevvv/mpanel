658c92c36e48d7d64e61efd3e651dfa73f91903c
Gayatri Dhatrak <dhatrak.gayatri@gmail.com>
2013-11-27 18:27:27 +0530

    Added condition to account brand page if the category does not exist

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,5 +1,4 @@
 class Attachment < ActiveRecord::Base
-
   attr_accessible :attachable_id, :attachable_type, :image,:image_cache
   mount_uploader :image, ImageUploader
   belongs_to :attachable, :polymorphic => true  
