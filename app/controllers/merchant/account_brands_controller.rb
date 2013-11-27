9d44cd7b2caaa6ce6603c5ea4c63068ded3aaae9
Amar Daxini <amardaxini@gmail.com>
2013-11-27 15:52:08 +0530

    fixed UI for account brand

--- app/controllers/merchant/account_brands_controller.rb
+++ app/controllers/merchant/account_brands_controller.rb
@@ -3,8 +3,9 @@ class Merchant::AccountBrandsController <  Merchant::BaseController
 
   def index 
     @brands = Brand.by_brand_name(params[:search_name]).where("brands.id not in (?)", @current_account.brands.pluck(:id))
-  	@account_brands = @current_account.brands
-  	@accounts = @current_account.account_brands
+  	@account_brands = @current_account.brands.order("brands.category_id asc")
+    
+  	# @accounts = @current_account.account_brands
     respond_to do |format|
       format.html # index.html.erb
     end
