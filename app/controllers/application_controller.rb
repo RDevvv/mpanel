3cc7a4b043e886cdd563a5b40d6e59ff6c5ece92
Gayatri Dhatrak <dhatrak.gayatri@gmail.com>
2013-11-27 13:10:07 +0530

    Fixed Login UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   layout :set_layout
 
-
   def after_sign_in_path_for(resource)
     # IF it's merchant redirect to merchant page
     if resource.is_a?(User)
@@ -12,10 +11,8 @@ class ApplicationController < ActionController::Base
     end
   end
   private
-  
 
   def set_layout
-
     if devise_controller? && devise_mapping.name == :merchant_user
       "merchant"
     else
