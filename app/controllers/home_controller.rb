98883d466d2203e701377fd487ceed52f3f5e553
vivek varade <varade.vivek.123@gmail.com>
2013-12-10 13:19:49 +0530

    location search working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,21 @@
 class HomeController < ApplicationController
-  def index
-  end
+    def index
+    end
 
-	def outlet_listing
-	@brands = Brand.all
-	end
+    def outlet_listing
+        result = Geocoder.search(params[:location]+" india")
+        unless result.empty?
+            @location = result.first.data["geometry"]["location"]
+            latitude = @location["lat"]
+            longitude = @location["lng"]
+            @outlet_versions = OutletVersion.new(:latitude => latitude, :longitude => longitude).nearbys(2)
+        else
+            @outlet_versions = nil
+        end
+    end
 
-	def map_listing
-        @brands = Brand.all
-	end
+    def map_listing
+        @outlets = Outlet.all
+    end
+
 end
--- app/models/outlet_version.rb
+++ app/models/outlet_version.rb
@@ -1,4 +1,11 @@
 class OutletVersion < ActiveRecord::Base
   attr_accessible :address, :area_id, :account_brand_id, :daily_cap, :email_id, :is_active, :is_deleted, :is_verified, :latitude, :longitude, :mobile_country_id, :mobile_number, :outlet_alias, :outlet_id, :outlet_key, :outlet_name, :outlet_type_id, :payment_value_id, :phone_number, :version_id
   belongs_to :outlet
+
+  after_validation :geocode
+  geocoded_by :geocoding_address
+
+  def geocoding_address
+      self.address
+  end
 end
