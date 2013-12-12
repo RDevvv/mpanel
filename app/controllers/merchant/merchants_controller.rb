e769adf277002ac5c1ce8272960247d23598d707
Amar Daxini <amardaxini@gmail.com>
2013-12-12 13:25:45 +0530

    replace compass css to static css

--- app/controllers/merchant/merchants_controller.rb
+++ app/controllers/merchant/merchants_controller.rb
@@ -4,6 +4,7 @@ class Merchant::MerchantsController < Merchant::BaseController
   
   def index
 	  if current_user
+      redirect_to merchant_accounts_path
 	  else
 	    redirect_to new_merchant_user_session_path
 	  end
