f08b46a0e781e864224090a5fd2cf6f5a7188e6a
Gayatri Dhatrak <dhatrak.gayatri@gmail.com>
2013-12-12 14:09:42 +0530

    After adding add user gets redirected to ad show page

--- app/controllers/merchant/ads_controller.rb
+++ app/controllers/merchant/ads_controller.rb
@@ -20,7 +20,7 @@ class Merchant::AdsController <  Merchant::BaseController
   def create
     @ad = @account_brand.ads.new(params[:ad])
     if @ad.save
-      redirect_to merchant_account_account_brand_ads_path(@current_account,@account_brand),:notice=>"Ad successfylly created"
+      redirect_to merchant_account_account_brand_ad_path(@current_account,@account_brand,@ad),:notice=>"Ad successfylly created"
     else
       @keywords = Keyword.all
       render :action=>"new"
