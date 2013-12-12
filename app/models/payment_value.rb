c7627c417a55477188b20f2cc746fb23e71c6261
Amar Daxini <amardaxini@gmail.com>
2013-12-12 14:46:12 +0530

    remove un wanted file

--- app/models/payment_value.rb
+++
@@ -1,4 +0,0 @@
-class PaymentValue < ActiveRecord::Base
-  attr_accessible :catchment_radius, :currency_id, :value
-  has_many :outlets, :dependent => :destroy
-end
--- test/unit/payment_value_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PaymentValueTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
