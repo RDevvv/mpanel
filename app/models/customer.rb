9bd7b19bfb3d9d44bb66aac5c9a5ee96315929b3
vivek varade <varade.vivek.123@gmail.com>
2013-12-14 15:22:58 +0530

    customer scaffold added, unique cookie generated

---
+++ app/models/customer.rb
@@ -0,0 +1,13 @@
+class Customer < ActiveRecord::Base
+    attr_accessible :uuid
+
+    def self.generate_cookie
+        customer_uuid = nil
+        while(customer_uuid == nil)
+            customer_uuid = SecureRandom.uuid
+            unless Customer.exists?(:uuid => customer_uuid)
+                return customer_uuid
+            end
+        end
+    end
+end
