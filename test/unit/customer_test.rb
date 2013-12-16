5fcdb68a1ab897d15648b039d6aa79b32efb17a6
vivek varade <varade.vivek.123@gmail.com>
2013-12-16 19:15:12 +0530

    generate_cookie method tested

--- test/unit/customer_test.rb
+++ test/unit/customer_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class CustomerTest < ActiveSupport::TestCase
+    test "check if cookies are generated" do
+        uuid = Customer.generate_cookie
+        assert_not_nil uuid
+    end
   # test "the truth" do
   #   assert true
   # end
