f49ead29760c0fb63fd6cd51c35a2fddbefb2f5e
vivek varade <varade.vivek.123@gmail.com>
2013-12-16 14:17:52 +0530

    address_button_select scaffold removed

--- app/models/address_button_select.rb
+++
@@ -1,4 +0,0 @@
-class AddressButtonSelect < ActiveRecord::Base
-  belongs_to :customer
-  # attr_accessible :title, :body
-end
--- test/unit/address_button_select_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AddressButtonSelectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
