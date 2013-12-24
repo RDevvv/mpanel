e544115e43b83cfed3dbf58b7b36238b05276277
vivek varade <varade.vivek.123@gmail.com>
2013-12-24 16:42:32 +0530

    outlet javascript moved to home.js

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,14 +1,22 @@
-var GL = GL || {};
-GL = (function($, window, document, Gullak){
-
-  Gullak.home = {
-    changeAccount : function(){
-      $("select#account_id").on("change",function(){
-        
-      })
+function track(element) {
+    check_class = $(element).attr("class");
+    if (check_class == "button_call2") {
+        console.log("within if condition");
+        $.ajax({
+            url: "get_call_click/".concat($.cookie("customer_uuid")).concat(".json")
+        }).done(function () {
+            console.log("vivekkkkkk");
+        })
     }
-  }
-  return Gullak;
-})(jQuery, this, this.document, GL);
+    else //if(check_class=="button_map2")
+        {
+            console.log("within else condition");
+            $.ajax({
+                url: "get_button_click/".concat($.cookie("customer_uuid")).concat("/").concat(check_class).concat(".json")
+                //add window.location.pathname to to given string
+            }).done(function () {
+            })
+        }
 
+}
 
