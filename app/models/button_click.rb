49c2ae6212b8d2d5f7e46144cceafeb142ae9923
vivek varade <varade.vivek.123@gmail.com>
2013-12-26 16:56:55 +0530

    button_click belongs to ad

--- app/models/button_click.rb
+++ app/models/button_click.rb
@@ -1,4 +1,6 @@
 class ButtonClick < ActiveRecord::Base
   belongs_to :customer
-  attr_accessible :ad_id, :button_class, :customer_id, :session_id
+  attr_accessible :ad_id, :button_class, :customer_id, :session_id, :current_link
+
+  belongs_to :ad
 end
