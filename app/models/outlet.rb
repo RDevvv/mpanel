d01790183aabb2c39bc8933424b0f1fdcbf3a296
Amar Daxini <amardaxini@gmail.com>
2013-12-27 15:21:56 +0530

    adde jquery validation emgine befor submitting import record

--- app/models/outlet.rb
+++ app/models/outlet.rb
@@ -19,8 +19,9 @@ class Outlet < ActiveRecord::Base
   validates :mobile_number, :format => { :with => /^[0-9\-\+]{9,15}$/,:message => "Invalid Mobile Number" } ,:allow_nil => true, :allow_blank => true
   validates :email_id, :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :message => "Invalid Email Id" } ,:allow_nil => true, :allow_blank => true
 	validates :phone_number,  :numericality => {:greater_than => 0, :message => " is an invalid number."}   ,:allow_nil => true, :allow_blank => true
-
+  validates_presence_of  :address
   validates_presence_of :account_brand, :area
+  
   after_save :geocode,:if => :address_changed?
   geocoded_by :geocoding_address
 
