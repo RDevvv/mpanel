032b826927f78b9ec142bced8815131841afcd6b
vivek varade <varade.vivek.123@gmail.com>
2013-12-30 18:54:54 +0530

    customer validation added

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,9 @@
 class Customer < ActiveRecord::Base
     has_many :facebook_shares
 
-    attr_accessible :uuid, :browser, :platform, :browser_version, :email_id, :name, :age, :gender, :date_of_birth, :incentive_count
+    attr_accessible :uuid, :mobile_number, :browser, :platform, :browser_version, :email_id, :name, :age, :gender, :date_of_birth, :incentive_count
+
+    validates :mobile_number, :uniqueness => true, :presence => true, :numericality => true,:length => {:minimum => 9, :maximum => 11}
 
     def self.generate_cookie
         customer_uuid = nil
