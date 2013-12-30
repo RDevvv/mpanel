9b3bee83ddb40ef5a4581288130c4eafb8883340
Amar Daxini <amardaxini@gmail.com>
2013-12-30 20:26:07 +0530

    updated migration

--- db/migrate/20131230144207_remove_unwanted_table.rb
+++ db/migrate/20131230144207_remove_unwanted_table.rb
@@ -1,6 +1,6 @@
 class RemoveUnwantedTable < ActiveRecord::Migration
   def up
-    drop_table :outlet_versions if ActiveRecord::Base.connection.table_exists? 'pincodes'
+    drop_table :outlet_versions if ActiveRecord::Base.connection.table_exists? 'outlet_versions'
     drop_table :ad_versions if ActiveRecord::Base.connection.table_exists? 'ad_versions'
     drop_table :area_pincodes if ActiveRecord::Base.connection.table_exists? 'area_pincodes'
   end
