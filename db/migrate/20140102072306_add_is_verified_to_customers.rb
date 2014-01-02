a1293afc9481a3ea9b5cb429786a38ba96b6bcac
vivek varade <varade.vivek.123@gmail.com>
2014-01-02 18:34:51 +0530

    migrations updated

--- db/migrate/20140102072306_add_is_verified_to_customers.rb
+++ db/migrate/20140102072306_add_is_verified_to_customers.rb
@@ -2,5 +2,7 @@ class AddIsVerifiedToCustomers < ActiveRecord::Migration
   def change
     add_column :customers, :is_verified, :boolean
     add_column :customers, :verification_code, :string
+    change_column :customers, :mobile_number, :integer, :limit => 8
+
   end
 end
