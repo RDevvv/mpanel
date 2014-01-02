1a97b0b3b857a9df94826c0ef9d0ccda26f5aab5
Amar Daxini <amardaxini@gmail.com>
2014-01-02 12:54:06 +0530

    added set name

--- app/models/ad_promocode.rb
+++ app/models/ad_promocode.rb
@@ -1,15 +1,14 @@
 class AdPromocode < ActiveRecord::Base
-  attr_accessible :ad_id, :cap, :is_used, :usage, :promocode, :outlet_ids, :promocodes, :is_active, :ad_group_id, :deleted_at
+  attr_accessible :ad_id, :cap, :is_used, :usage, :promocode, :outlet_ids, :promocodes, :is_active, :ad_group_id, :deleted_at,:set_name
   attr_accessor :promocodes
   belongs_to :ad
   belongs_to :ad_group
   has_many :ad_promocode_outlets,:dependent=>:destroy
   has_many :outlets,:through=>:ad_promocode_outlets
   validates :promocode, :format => { :with => /\A[a-zA-Z0-9]+\z/,:message => "Invalid Promocode" } 
-  validates_presence_of :set_name
   before_destroy :can_delete
   after_save :inactive_ad_promocode
-
+  attr_accessor :set_name
   acts_as_paranoid
     
   def can_delete
