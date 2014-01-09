3d6642b4f5d70fb913233f94079055f9cc34754b
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-01-09 13:49:28 +0530

    lat and long removed

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,7 @@ class Customer < ActiveRecord::Base
     has_many :customer_sessions
     has_many :sms_sents
 
-    attr_accessible :uuid, :mobile_number, :browser, :platform, :browser_version, :email_id, :name, :age, :gender, :latitude, :longitude
+    attr_accessible :uuid, :mobile_number, :browser, :platform, :browser_version, :email_id, :name, :age, :gender
     attr_accessible :date_of_birth, :incentive_count, :verification_code, :is_verified
 
     validates :mobile_number, :uniqueness => true, :numericality => true,:length => {:minimum => 9, :maximum => 11}, :allow_blank => true
