f892ff5e21cc5b39141e4f72c7889b78910ff3b5
Gayatri Dhatrak <dhatrak.gayatri@gmail.com>
2014-01-09 14:36:16 +0530

    Seeds file edited

--- db/migrate/20140109074150_add_latitude_and_longitude_to_customer_sessions.rb
+++
@@ -1,6 +0,0 @@
-class AddLatitudeAndLongitudeToCustomerSessions < ActiveRecord::Migration
-  def change
-    add_column :customer_sessions, :longitude, :float
-    add_column :customer_sessions, :latitude, :float
-  end
-end
