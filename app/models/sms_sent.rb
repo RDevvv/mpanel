cf22a31327c2318f43b7aa83dfe05ac50c2b93f3
vivek varade <varade.vivek.123@gmail.com>
2014-01-14 18:43:10 +0530

    sms method added to sms_sent

--- app/models/sms_sent.rb
+++ app/models/sms_sent.rb
@@ -1,9 +1,24 @@
 class SmsSent < ActiveRecord::Base
-  belongs_to :customer
-  belongs_to :ad_promocode_outlet
+    belongs_to :customer
+    belongs_to :ad_promocode_outlet
 
-  attr_accessible :text, :is_sent
+    attr_accessible :text, :is_sent
 
-  validates :text, :presence => true
-  validates :customer_id, :presence => true
+    validates :text, :presence => true
+    validates :customer_id, :presence => true
+
+    def message(phones, message)
+        mobilenumbers = phones.join(",")
+        url = URI.parse("http://www.smscountry.com/smscwebservice_bulk.aspx")
+        request = Net::HTTP::Post.new(url.path)
+        request.set_form_data({'user' => "gullak", 'passwd' => "babu3384ad", 'mobilenumber' => mobilenumbers, 'message' => message, 'senderid' => "Gullake", 'mtype' => "N", 'DR' => "Y"})
+        response = Net::HTTP.new(url.host, url.port).start {|http| http.request(request) }
+        case response
+        when Net::HTTPSuccess
+            puts response.body
+        else
+            response.body
+            response.error!
+        end
+    end
 end
