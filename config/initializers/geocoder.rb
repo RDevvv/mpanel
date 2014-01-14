981ee12c021e73d79c80d37c54e2c80de49aca84
vivek varade <varade.vivek.123@gmail.com>
2014-01-14 15:58:07 +0530

    rails cache used for geocoding

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -9,7 +9,7 @@ Geocoder.configure(
   # :use_https    => false,       # use HTTPS for lookup requests? (if supported)
   # :http_proxy   => nil,         # HTTP proxy server (user:pass@host:port)
   # :https_proxy  => nil,         # HTTPS proxy server (user:pass@host:port)
-  # :cache        => nil,         # cache object (must respond to #[], #[]=, and #keys)
+   :cache        => Rails.cache, # cache object (must respond to #[], #[]=, and #keys)
   # :cache_prefix => "geocoder:", # prefix (string) to use for all cache keys
 
   # exceptions that should not be rescued by default
