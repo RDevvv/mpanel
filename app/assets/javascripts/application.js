2da5f0a71b4631b2c989dc1851fef3cb8a955a0f
vivek varade <varade.vivek.123@gmail.com>
2014-01-15 10:43:00 +0530

    bootstrap multiple occurences removed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require bootstrap
 //= require jquery.pnotify.min
 //= require jquery.mmenu
 //= require jquery.cookie
