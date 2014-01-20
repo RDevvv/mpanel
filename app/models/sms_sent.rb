454db93ab64ecba37745ac76cb1232296cd1a1fc
vivek varade <varade.vivek.123@gmail.com>
2014-01-20 20:01:36 +0530

    after every ad_sent usage will increase in ads table

--- app/models/sms_sent.rb
+++ app/models/sms_sent.rb
@@ -9,6 +9,13 @@ class SmsSent < ActiveRecord::Base
     validates :ad_promocode_outlet_id, :presence => true
     validates :ad_promocode_outlet_version_id, :presence => true
 
+    after_create :track_usage
+
+    def track_usage
+        ad = self.ad_promocode_outlet.ad
+        ad.update_attributes(:usage => (ad.usage+1))
+    end
+
     def message(number, message)
         message = Curl::Easy.new("https://gullakmaster:449df2572ff8b57017ceb975d5dc15b93d480e02@twilix.exotel.in/v1/Accounts/gullakmaster/Sms/send")
         message.ssl_verify_peer = false
