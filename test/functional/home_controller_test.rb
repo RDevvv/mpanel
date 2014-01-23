6771aaa7b1fed45dd5ca193fe178c68d4c7ee03e
vivek varade <varade.vivek.123@gmail.com>
2014-01-23 12:00:12 +0530

    home tests improved

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -9,17 +9,12 @@ class HomeControllerTest < ActionController::TestCase
   end
 
   test "should get map_listing" do
-    get :map_listing
+    get :map_listing, :location => "bandra"
     assert_response :success
   end
 
   test "should get outlet_listing" do
     get :outlet_listing, :location => "bandra"
-    assert_response :success
-  end
-
-  test "should get individual_outlet" do
-    get :individual_outlet, :id => 1
     assert_response :success
   end
 end
