6b0cca22f259c5008bc2cc16508f6b7218617c67
vivek varade <varade.vivek.123@gmail.com>
2014-01-23 12:18:28 +0530

    tests updated

--- test/functional/button_clicks_controller_test.rb
+++ test/functional/button_clicks_controller_test.rb
@@ -9,11 +9,11 @@ class ButtonClicksControllerTest < ActionController::TestCase
         @button_click = button_clicks(:one)
     end
 
-    test "should create button_click" do
+    test "should trigger button_click" do
         assert_difference('ButtonClick.count') do
             get :get_click, customer_uuid: 'a1825be5-36b7-40dd-9e06-0bf58920235d' , button_click: { ad_id: @button_click.ad_id, button_class: @button_click.button_class}
         end
 
-        assert_redirected_to button_click_path(assigns(:button_click))
+        assert_response :success
     end
 end
