d7ee8b94a96e04688367ab6622c29b447cb140f1
vivek varade <varade.vivek.123@gmail.com>
2014-01-24 19:55:05 +0530

    debuging removed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,6 @@ Gullak2::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
   config.assets.logger = Logger.new($stdout)
-  config.log_level = :debug
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
