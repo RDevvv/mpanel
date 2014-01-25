f3a544a32a0bcf2c7ee5e408267233331489a4b4
vivek varade <varade.vivek.123@gmail.com>
2014-01-25 18:15:49 +0530

    map views updated for previous refactoring

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -51,10 +51,10 @@ class HomeController < ApplicationController
     end
 
     def map_listing
-        location = Outlet.get_coordinates(params[:location],params[:longitude], params[:latitude])
+        @location = Outlet.get_coordinates(params[:location],params[:longitude], params[:latitude])
 
-        Customer.where(:uuid =>cookies[:customer_uuid]).first.customer_sessions.last.update_attributes(:latitude => location[:latitude], :longitude => location[:longitude])
-        @outlets = Outlet.new(:latitude => location[:latitude], :longitude => location[:longitude]).nearbys(5, :units => :km)
+        Customer.where(:uuid =>cookies[:customer_uuid]).first.customer_sessions.last.update_attributes(:latitude => @location[:latitude], :longitude => @location[:longitude])
+        @outlets = Outlet.new(:latitude => @location[:latitude], :longitude => @location[:longitude]).nearbys(5, :units => :km)
         @final_outlets = Outlet.sort_outlet_by_ad_presence(@outlets)
         #@final_outlets = Kaminari.paginate_array(@final_outlets).page(params[:page]).per(5)
     end
@@ -99,14 +99,14 @@ class HomeController < ApplicationController
     end
 
     def map_search
-        location = Outlet.get_coordinates(params[:location],params[:longitude], params[:latitude])
+        @location = Outlet.get_coordinates(params[:location],params[:longitude], params[:latitude])
 
         r = Keyword.search do
             fulltext params[:search]
         end
 
-        Customer.where(:uuid =>cookies[:customer_uuid]).first.customer_sessions.last.update_attributes(:latitude => location[:latitude], :longitude => location[:longitude])
-        @outlets = Outlet.new(:latitude => location[:latitude], :longitude => location[:longitude]).nearbys(5, :units => :km)
+        Customer.where(:uuid =>cookies[:customer_uuid]).first.customer_sessions.last.update_attributes(:latitude => @location[:latitude], :longitude => @location[:longitude])
+        @outlets = Outlet.new(:latitude => @location[:latitude], :longitude => @location[:longitude]).nearbys(5, :units => :km)
         unless @outlets.empty?
             @nearby_outlets = @outlets.map{|o| o.id}.uniq
         else
