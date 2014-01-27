cc878a29266cdc4dd4964cbde5f97e8ed264c2ea
vivek varade <varade.vivek.123@gmail.com>
2014-01-27 15:35:43 +0530

    tests updated

--- test/unit/call_forwarding_test.rb
+++ test/unit/call_forwarding_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class CallForwardingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "should get customer id" do
+        CallForwarding.first.get_customer_id
+        assert_equal Customer.first.id, CallForwarding.first.customer_id
+    end
+
+    test "should compare call forwarding number with exotel" do
+        #CallForwarding.compare_call_forwarding_number_with_exotel
+    end
 end
--- test/unit/campaign_test.rb
+++ test/unit/campaign_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class CampaignTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "if campaign generates url for sharing" do
+        url = Campaign.generate_url(AdPromocodeOutlet.first)
+        assert_equal url, "http://www.gullakmaster.com//deals/organic/facebook/Mumbai/nerul/Spa/1/"
+    end
 end
