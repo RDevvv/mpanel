00652eb83c294d4b9c487e6d8a181ac46ddf5685
vivek varade <varade.vivek.123@gmail.com>
2014-01-27 13:34:17 +0530

    tests updated

--- test/unit/customer_session_test.rb
+++ test/unit/customer_session_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class CustomerSessionTest < ActiveSupport::TestCase
     test "if gecoding function returns address" do
         address = CustomerSession.first.geocoding_address
-        assert_equals "india", address
+        assert_equal "india", address
     end
 end
