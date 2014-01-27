45d48ff016b5788bc9209597ebab6a28311fd492
vivek varade <varade.vivek.123@gmail.com>
2014-01-27 16:16:36 +0530

    location cache removed

--- app/models/location_cache.rb
+++
@@ -1,7 +0,0 @@
-class LocationCache < ActiveRecord::Base
-  attr_accessible :latitude, :location, :longitude
-
-  validates :latitude, :presence => true
-  validates :longitude, :presence => true
-  validates :location, :presence => true
-end
