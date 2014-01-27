a5cf02e94adb202ae38292e9bd891f8be6ba2e45
vivek varade <varade.vivek.123@gmail.com>
2014-01-27 19:43:23 +0530

    unneccessary method removed from merchant

--- app/helpers/merchant_helper.rb
+++ app/helpers/merchant_helper.rb
@@ -1,18 +1,4 @@
 module MerchantHelper
-  def active_class_for_sidebar(class_name="home")
-
-    classes = {
-      "accounts"=>"account",
-      "account_brands"=>"brand",
-      "brands"=>"brand",
-    }
-    if class_name == (classes[controller.controller_name + '.' + controller.action_name] || classes[controller.controller_name] || '')
-      "btn-primary" 
-    else
-      "btn-success" 
-    end
-  end
-
   def no_sidebar
     classes = {"accounts.new"=>"no_sidebar","accounts.verified_account"=>"no_sidebar"}
       
