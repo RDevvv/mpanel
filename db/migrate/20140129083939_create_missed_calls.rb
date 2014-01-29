19fe40150571dc54b6100b561b50887da30af9e5
vivek varade <varade.vivek.123@gmail.com>
2014-01-29 15:20:58 +0530

    missed_call column type changed

--- db/migrate/20140129083939_create_missed_calls.rb
+++ db/migrate/20140129083939_create_missed_calls.rb
@@ -3,8 +3,8 @@ class CreateMissedCalls < ActiveRecord::Migration
     create_table :missed_calls do |t|
       t.references :campaign
       t.string :call_sid
-      t.integer :from, :limit => 8
-      t.integer :to, :limit => 8
+      t.string :from
+      t.string :to
       t.boolean :is_valid
 
       t.timestamps
