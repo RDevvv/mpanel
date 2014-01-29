4365b6e69fb279174edc6a652ed07b9384d43909
vivek varade <varade.vivek.123@gmail.com>
2014-01-29 19:08:27 +0530

    missed call controller added

--- app/controllers/missed_calls_controller.rb
+++ app/controllers/missed_calls_controller.rb
@@ -1,6 +1,15 @@
 class MissedCallsController < ApplicationController
     def get_missed_call
-        @missed_call = MissedCall.create(:call_sid=>params["CallSid"],:from=>params[:CallFrom],:to=>params["CallTo"])
+        unless MissedCall.exists?(:from => params[:CallFrom])
+            customer = Customer.create(:mobile_number => params[:CallFrom], :uuid => Customer.generate_cookie)
+            campaign = Campaign.where(:marketer => "GM Marketer", :target => "customer", :source => "exotel", :medium => "SMS", :type => "auto trigger", :purpose => "acquisition")
+            @missed_call = MissedCall.create(:call_sid=>params["CallSid"],:from=>params[:CallFrom],:to=>params["CallTo"], :campaign_id => campaign.id)
+            CampaignCopy.create(:vendor_id => params["CallSid"], :customer_id => customer.id)
+        end
         render :json => 'OK', :status => '200'
+    end
+
+    def self.add_exotel_campaign()
+            Campaign.create(:marketer => "GM Marketer", :target => "customer", :source => "exotel", :medium => "SMS", :type => "auto trigger", :purpose => "acquisition", :campaign_template => "Your contest entry is confirmed. We will call the lucky winner of Samsung Grand 2 on 14/02/14. Try gullak.co/xxxx for more Deals At Your Fingertips")
     end
 end
