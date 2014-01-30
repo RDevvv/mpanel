515a92bf2a081ba6346c73fff06727e386035620
Gayatri Dhatrak <dhatrak.gayatri@gmail.com>
2014-01-30 18:48:26 +0530

    Added functional tests

--- test/functional/merchant/accounts_controller_test.rb
+++ test/functional/merchant/accounts_controller_test.rb
@@ -5,38 +5,57 @@ class Merchant::AccountsControllerTest < ActionController::TestCase
 
   setup do
     @user = users(:one)
-    sign_in @user
     @account = accounts(:one)
+    sign_in @user
   end
 
   test "should get index" do
-      get :index
-      assert_response :success
+    get :index
+    assert_response :success
   end
 
- # test "should get new" do
- #     get :new
- #     assert_response :success
- # end
+  # test "should get new" do
+  #   get :new
+  #   assert_response :success
+  # end
 
- # test "should get edit" do
- #     get :edit, id: @account
- #     assert_response :success
- # end
+  # test "should get edit" do
+  #   get :edit, id: 1
+  #   assert_response :success
+  # end
 
- # test "should destroy an account" do
- #   assert_difference('Account.count', -1) do
- #     delete :destroy, id: @account
- #   end
+  # test "should create new account" do
+  #   assert_difference('Account.count') do
+  #     post :create, account: { address: @account.address, area_id: @account.area_id, registered_company_name: @account.registered_company_name }
+  #   end
+  # end
 
- #   assert_redirected_to accounts_path
- # end
+  # test "should update an account" do
+  #   put :update, account: { address: @account.address, area_id: @account.area_id, registered_company_name: @account.registered_company_name }
+  #   assert_redirected_to merchant_accounts_path
+  # end
 
- # test "should verify accounts" do
- #     get :verified_account, id: 1
- #     assert_response :success
- # end
+  # test "should destroy an account" do
+  #   assert_difference('Account.count', -1) do
+  #     delete :destroy, id: @account
+  #   end
 
- # test "should add brands" do
- # end
+  #   assert_redirected_to accounts_path
+  # end
+
+  # test "should get show" do
+  #   get :show, id: 1
+  #   assert_response :success
+  # end
+
+  # test "should verify accounts" do
+  #   get :verified_account, id: 1
+  #   assert_response :success
+  # end
+
+  # test "should add brands" do
+  #   post :add_brands, id:1
+  #   assert_response :success
+  # end
+
 end
--- test/functional/merchant/ad_groups_controller_test.rb
+++ test/functional/merchant/ad_groups_controller_test.rb
@@ -1,16 +1,21 @@
 require 'test_helper'
 
 class Merchant::AdGroupsControllerTest < ActionController::TestCase
-    include Devise::TestHelpers
+  include Devise::TestHelpers
 
-    setup do
-        @user = users(:one)
-        sign_in @user
-    end
+  setup do
+    @user = users(:one)
+    sign_in @user
+  end
 
-    test "should show ad group" do
-        get :show, account_id: 1, account_brand_id: 1, ad_id:1, id:1
-        assert_response :success
-    end
+  test "should show ad group" do
+    get :show, account_id: 1, account_brand_id: 1, ad_id:1, id:1
+    assert_response :success
+  end
+
+  test "should delete outlet" do
+    get :delete_outlet, account_id: 1, account_brand_id: 1, ad_id:1, id:1
+    assert_response :success
+  end
 
 end
