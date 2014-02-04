269ad164b1598e7f91f0168c86851222a7616eb9
vivek varade <varade.vivek.123@gmail.com>
2014-02-04 15:40:15 +0530

    lowercase search added

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -10,7 +10,7 @@ class Keyword < ActiveRecord::Base
     validates_uniqueness_of :keyword, :message => " Oops...This keyword already exists."
 
     def self.search(term)
-        result = Keyword.where("keyword like '%#{term}%'") 
+        result = Keyword.where("LOWER(keyword) like '%#{term.downcase}%'") 
         unless result.blank?
             final_result = result.first.ad_keywords.map{|ak| ak.ad}
         end
