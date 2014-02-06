d6cd59599e234825f1183303d8b48c2720faed83
vivek varade <varade.vivek.123@gmail.com>
2014-02-06 20:14:44 +0530

    missed calls associated with the campaign

--- app/controllers/missed_calls_controller.rb
+++ app/controllers/missed_calls_controller.rb
@@ -3,9 +3,9 @@ class MissedCallsController < ApplicationController
         unless MissedCall.exists?(:from => params[:From])
             customer = Customer.create(:mobile_number => params[:From], :uuid => Customer.generate_cookie)
             template = "Your contest entry is confirmed. We will call the lucky winner of Samsung Grand 2 on 14/02/14. Try http://gullak.co/xxxx for more Deals At Your Fingertips"
-            campaign = Campaign.create(:marketer => "GM Marketer", :target => "customer", :source => "exotel", :medium => "SMS", :campaign_type => "auto trigger", :purpose => "acquisition", :campaign_template => template)
+            campaign = Campaign.where(:source => "Exotel", :medium => "SMS").first
             @missed_call = MissedCall.create(:call_sid=>params["CallSid"],:from=>params[:From],:to=>params["To"], :campaign_id => campaign.id)
-            CampaignCopy.create(:vendor_id => params["CallSid"], :customer_id => customer.id)
+            campaign.campaign_copies.create(:vendor_id => params["CallSid"], :customer_id => customer.id)
         end
         render :json => 'OK', :status => '200'
     end
