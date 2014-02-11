526567dae1cc2b18ec023c3550fba62dd0248c48
vivek varade <varade.vivek.123@gmail.com>
2014-02-11 19:21:39 +0530

    tests updated

--- test/unit/ad_promocode_test.rb
+++ test/unit/ad_promocode_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class AdPromocodeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "if ad group is added" do
+        AdPromocode.find(1).add_ad_group(Ad.find(1), "new group")
+        assert_equal Ad.find(1).ad_groups.last.name, "new group"
+    end
+
+    test "if ad_id is added to ad_promocode from ad_group" do
+        AdPromocode.find(1).add_ad
+        assert_equal AdPromocode.find(1).ad_id, AdPromocode.find(1).ad_group.ad.id
+    end
 end
--- test/unit/outlet_test.rb
+++ test/unit/outlet_test.rb
@@ -28,4 +28,14 @@ class OutletTest < ActiveSupport::TestCase
         geocoding_address = Outlet.first.geocoding_address
         assert_equal "golibar road, bandra, Mumbai", geocoding_address
     end
+
+    test "test if get_address is working" do
+        address = Outlet.find(1).get_address
+        assert_equal "32 golibar road, bandra, Mumbai, 400706", address
+    end
+
+    test "if sort_by_brands is working" do
+        #sorted_outlets = Outlet.find(1).sort_by_brands(1)
+        #assert_equal "vivek", sorted_outlets
+    end
 end
