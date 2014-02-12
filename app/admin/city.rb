48e587f1fea22bf0491baca17433ed153677cf6a
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-02-12 13:57:07 +0530

    city#index on admin_panel improved

--- app/admin/city.rb
+++ app/admin/city.rb
@@ -1,3 +1,17 @@
 ActiveAdmin.register City do
-
+  index do
+    column :id
+    column :city_name
+    column :time_zone
+    column :created_at
+    column :updated_at
+    column :latitude
+    column :longitude
+    actions :defaults => false do |city|
+      link_to "View", admin_city_path(city)
+    end
+    actions :defaults => false do |city|
+      link_to "update", edit_admin_city_path(city)
+    end
+  end
 end
