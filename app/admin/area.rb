4cd31fb24706141c57602ce1a7b4fe1ec1a5ea3b
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-02-12 13:51:38 +0530

    area#index on admin_panel improved

--- app/admin/area.rb
+++ app/admin/area.rb
@@ -1,3 +1,17 @@
 ActiveAdmin.register Area do
-
+  index do
+    column :id
+    column :area_name
+    column :created_at
+    column :updated_at
+    column :latitude
+    column :longitude
+    column :pincode
+    actions :defaults => false do |area|
+      link_to "View", admin_area_path(area)
+    end
+    actions :defaults => false do |area|
+      link_to "update", edit_admin_area_path(area)
+    end
+  end
 end
