f27247a93ab5423cb1bbef2c509bcc1c7bc679fb
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-02-14 13:06:37 +0530

    category on admin_panel improved

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -1,3 +1,38 @@
 ActiveAdmin.register Category do
 
+  index do
+    column :id
+    column :category_name
+    column :industry do |category|
+      category.industry.industry_name
+    end
+    actions :defaults => false do |category|
+      link_to "View", admin_category_path(category)
+    end
+    actions :defaults => false do |category|
+      link_to "update", edit_admin_category_path(category)
+    end
+  end
+
+  form do |f|
+    f.inputs  do
+      f.input :category_name, :as => :string
+      f.input :industry, :as => :select, :collection => Industry.all.map {|u| [u.industry_name, u.id]}
+    end
+    f.actions
+  end
+
+  show do |category|
+    attributes_table do
+      row :id
+      row :category_name
+      row :industry do |category|
+        category.industry.industry_name
+      end
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
+
 end
