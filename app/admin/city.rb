d66b0538907ca693e61ef9efb8fb349d2b85fd0a
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-02-14 18:46:46 +0530

    latitude and longitude removed from city admin_panel

--- app/admin/city.rb
+++ app/admin/city.rb
@@ -3,8 +3,7 @@ ActiveAdmin.register City do
     column :id
     column :city_name
     column :time_zone
-    column :latitude
-    column :longitude
+    column :country_id
     actions :defaults => false do |city|
       link_to "View", admin_city_path(city)
     end
@@ -12,4 +11,26 @@ ActiveAdmin.register City do
       link_to "update", edit_admin_city_path(city)
     end
   end
+
+  form do |f|
+    f.inputs do
+      f.input :city_name
+      f.input :time_zone
+      f.input :country
+    end
+    f.actions
+  end
+
+  show do |city|
+    attributes_table do
+      row :id
+      row :city_name
+      row :time_zone
+      row :country
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
+
 end
