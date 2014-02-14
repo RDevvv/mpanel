ff91b32ef3c89d7331a8a2697f83f85e38def8af
vivek varade <varade.vivek.123@gmail.com>
2014-02-14 11:58:04 +0530

    campaigns controller refactored

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,122 +1,96 @@
 class CampaignsController < ApplicationController
-  # GET /campaigns
-  # GET /campaigns.json
-  def index
-    @campaigns = Campaign.all
+    respond_to :html, :json
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @campaigns }
+    # GET /campaigns
+    # GET /campaigns.json
+    def index
+        @campaigns = Campaign.all
+
+        respond_with @campaigns
     end
-  end
 
-  # GET /campaigns/1
-  # GET /campaigns/1.json
-  def show
-    @campaign = Campaign.find(params[:id])
+    # GET /campaigns/1
+    # GET /campaigns/1.json
+    def show
+        @campaign = Campaign.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @campaign }
+        respond_with @campaign
     end
-  end
 
-  # GET /campaigns/new
-  # GET /campaigns/new.json
-  def new
-    @campaign = Campaign.new
+    # GET /campaigns/new
+    # GET /campaigns/new.json
+    def new
+        @campaign = Campaign.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @campaign }
+        respond_with @campaign
     end
-  end
 
-  # GET /campaigns/1/edit
-  def edit
-    @campaign = Campaign.find(params[:id])
-  end
-
-  # POST /campaigns
-  # POST /campaigns.json
-  def create
-    @campaign = Campaign.new(params[:campaign])
-
-    respond_to do |format|
-      if @campaign.save
-        format.html { redirect_to @campaign, notice: 'Campaign was successfully created.' }
-        format.json { render json: @campaign, status: :created, location: @campaign }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @campaign.errors, status: :unprocessable_entity }
-      end
+    # GET /campaigns/1/edit
+    def edit
+        @campaign = Campaign.find(params[:id])
     end
-  end
 
-  # PUT /campaigns/1
-  # PUT /campaigns/1.json
-  def update
-    @campaign = Campaign.find(params[:id])
+    # POST /campaigns
+    # POST /campaigns.json
+    def create
+        @campaign = Campaign.new(params[:campaign])
 
-    respond_to do |format|
-      if @campaign.update_attributes(params[:campaign])
-        format.html { redirect_to @campaign, notice: 'Campaign was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @campaign.errors, status: :unprocessable_entity }
-      end
+        respond_with @campaign, :location => campaigns_path
     end
-  end
 
-  # DELETE /campaigns/1
-  # DELETE /campaigns/1.json
-  def destroy
-    @campaign = Campaign.find(params[:id])
-    @campaign.destroy
+    # PUT /campaigns/1
+    # PUT /campaigns/1.json
+    def update
+        @campaign = Campaign.find(params[:id])
 
-    respond_to do |format|
-      format.html { redirect_to campaigns_url }
-      format.json { head :no_content }
+        respond_with @campaign, :location => campaigns_path
     end
-  end
 
-  def facebook_share
-      if params[:short_url].nil?
-          @ad_promocode_outlet = AdPromocodeOutlet.find(params[:ad_promocode_outlet_id])
-      else
-          @campaign = Campaign.where(:short_url => params[:short_url]).first
-          @ad_promocode_outlet = @campaign.ad_promocode_outlet
-      end
-      @brand = @ad_promocode_outlet.ad.account_brand.brand
-  end
+    # DELETE /campaigns/1
+    # DELETE /campaigns/1.json
+    def destroy
+        @campaign = Campaign.find(params[:id])
+        @campaign.destroy
 
-  def campaign_landing
-      campaign = Campaign.where(:short_url => params[:short_url])
-      unless campaign.blank?
-          campaign = campaign.first
-          campaign.campaign_copies.create
-          redirect_to campaign.pre_expiry_forward_url
-      else
-          campaign_copy = CampaignCopy.where(:short_url => params[:short_url]).first
-          use_count = campaign_copy.use_count
-          if use_count.blank?
-              campaign_copy.update_attributes(:use_count => 0)
-          end
-          CampaignCopy.where(:short_url => params[:short_url]).first.update_attributes(:use_count=> campaign_copy.use_count+1)
-          if campaign_copy.expires_at < Date.today-1
-              redirect_to campaign_copy.campaign.pre_expiry_forward_url
-          else
-              redirect_to campaign_copy.campaign.post_expiry_forward_url
-          end
-      end
-  end
+        respond_with @campaign
+    end
 
-  def generate_campaign_copy
-      campaign_copy = Ad.find(params[:ad_id]).ad_promocode_outlets.where(:outlet_id => params[:outlet_id]).first.campaign.campaign_copies.create()
+    def facebook_share
+        if params[:short_url].nil?
+            @ad_promocode_outlet = AdPromocodeOutlet.find(params[:ad_promocode_outlet_id])
+        else
+            @campaign = Campaign.where(:short_url => params[:short_url]).first
+            @ad_promocode_outlet = @campaign.ad_promocode_outlet
+        end
+        @brand = @ad_promocode_outlet.ad.account_brand.brand
+    end
 
-      respond_to do |format|
-          format.json {render :json => {:short_url => campaign_copy.short_url}}
-      end
-  end
+    def campaign_landing
+        campaign = Campaign.where(:short_url => params[:short_url])
+        unless campaign.blank?
+            campaign = campaign.first
+            campaign.campaign_copies.create
+            redirect_to campaign.pre_expiry_forward_url
+        else
+            campaign_copy = CampaignCopy.where(:short_url => params[:short_url]).first
+            use_count = campaign_copy.use_count
+            if use_count.blank?
+                campaign_copy.update_attributes(:use_count => 0)
+            end
+            CampaignCopy.where(:short_url => params[:short_url]).first.update_attributes(:use_count=> campaign_copy.use_count+1)
+            if campaign_copy.expires_at < Date.today-1
+                redirect_to campaign_copy.campaign.pre_expiry_forward_url
+            else
+                redirect_to campaign_copy.campaign.post_expiry_forward_url
+            end
+        end
+    end
+
+    def generate_campaign_copy
+        campaign_copy = Ad.find(params[:ad_id]).ad_promocode_outlets.where(:outlet_id => params[:outlet_id]).first.campaign.campaign_copies.create()
+
+        respond_with do |format|
+            format.json {render :json => {:short_url => campaign_copy.short_url}}
+        end
+    end
 end
