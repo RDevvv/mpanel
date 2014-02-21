2c0d97d4b0905c148ca98bb10206983f49084bb3
vivek varade <varade.vivek.123@gmail.com>
2014-02-21 14:24:18 +0530

    javascript updated for tracking

--- app/assets/javascripts/binding_tags.js
+++ app/assets/javascripts/binding_tags.js
@@ -2,24 +2,33 @@ function binding_tags(tracking){
     sms_share_link = $('.sms_share_link');
     sms_share_link = $.makeArray(sms_share_link);
 
+    email_share_link = $('.email_share');
+    email_share_link = $.makeArray(email_share_link);
+
     call_buttons = $('.call');
     call_buttons = $.makeArray(call_buttons);
 
     ad_request = $('.ad_request');
     ad_request = $.makeArray(ad_request);
 
-    if(tracking == true)
-        {
-            address = $('.address');
-            address = $.makeArray(address);
+    view_buttons = $('.button_view');
+    view_buttons = $.makeArray(view_buttons);
 
-            description = $('.description');
-            description = $.makeArray(description);
+    share_buttons = $('.share');
+    share_buttons = $.makeArray(share_buttons);
 
-            all_buttons = call_buttons.concat(ad_request).concat(description).concat(address).concat(sms_share_link);
-        }
-        else{
-            all_buttons = call_buttons.concat(ad_request).concat(sms_share_link);
-        }
-        return all_buttons;
+    if(tracking == true){
+        address = $('.address');
+        address = $.makeArray(address);
+
+        description = $('.description');
+        description = $.makeArray(description);
+
+        all_buttons = call_buttons.concat(ad_request).concat(description).concat(address).concat(sms_share_link).concat(view_buttons).concat(share_buttons).concat(email_share_link);
+        console.log(view_buttons);
+    }
+    else{
+        all_buttons = call_buttons.concat(ad_request).concat(sms_share_link);
+    }
+    return all_buttons;
 }
