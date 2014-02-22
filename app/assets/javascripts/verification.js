79ca760867557b0016a9b2e1488588ca07d3f0b7
vivek varade <varade.vivek.123@gmail.com>
2014-02-22 13:57:24 +0530

    verification and ad_sent updated

--- app/assets/javascripts/verification.js
+++ app/assets/javascripts/verification.js
@@ -3,6 +3,10 @@ function mobile_number_verification(){
     all_buttons = binding_tags(binding_tags_for_tracking);
     $(all_buttons).click(function(){
         if($.cookie("mobile_number") == 'verified'){}
+        else if($.cookie("mobile_number") == 'not_verified'){
+            $('#verification').modal('show');
+            $('#popup_share').modal('hide');
+        }
         else if(($.cookie("mobile_number") == 'false')||($.cookie('mobile_number'))||(typeof($.cookie('mobile_number')) == 'undefined')){
             $('#mobile-number').modal('show');
             $('#popup_share').modal('hide');
@@ -49,20 +53,28 @@ function modal_submit(){
         }).success(function(data){
             data = JSON.parse(data);
             if(data["mobile_number"]==1)
-            {
-                $('#mobile-number').modal('hide');
-                $('#verification').modal('show');
-            }
-            else if(data["mobile_number"]=="exist")
-            {
-                $.pnotify({ title: '', text: 'Welcome back', closer_hover: false, sticker_hover: false, icon: false, opacity: .9 });
-                $('#mobile-number').modal('hide');
-                window.location.reload();
-            }
-            else
-            {
-                $.pnotify({ title: '', text: 'Enter a valid mobile number', closer_hover: false, sticker_hover: false, icon: false, opacity: .9 });
-            }
+                {
+                    $('#mobile-number').modal('hide');
+                    $('#verification').modal('show');
+                }
+                else if(data["mobile_number"]=="exist")
+                    {
+                        if($.cookie('mobile_number')=='not_verified'){
+                            console.log('iffffffffffff');
+                            $('#verification').modal('show');
+                            $('#mobile-number').modal('hide');
+                        }
+                        else{
+                            console.log('elseeeeeeeeeeeeeeeeeee');
+                            $.pnotify({ title: '', text: 'Welcome back', closer_hover: false, sticker_hover: false, icon: false, opacity: .9 });
+                            $('#mobile-number').modal('hide');
+                            window.location.reload();
+                        }
+                    }
+                    else
+                        {
+                            $.pnotify({ title: '', text: 'Enter a valid mobile number', closer_hover: false, sticker_hover: false, icon: false, opacity: .9 });
+                        }
         });
         return false; // prevents normal behaviour
     });
