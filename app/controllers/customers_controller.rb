fa84da92f0d48663a3b8fde6ffb3883c3b3f7021
vivek varade <varade.vivek.123@gmail.com>
2014-02-22 10:24:21 +0530

    fullstop removed from verification message

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -107,7 +107,7 @@ class CustomersController < ApplicationController
       else
           unless params[:mobile_number].empty?
               if @customer.update_attributes(:mobile_number => params[:mobile_number])
-                  @sms_sent = @customer.sms_sents.create(:ad_promocode_outlet_id => 1, :ad_promocode_outlet_version_id => 1, :text => "Your verification code is #{@customer.verification_code} Thanks, GullakMaster.")
+                  @sms_sent = @customer.sms_sents.create(:ad_promocode_outlet_id => 1, :ad_promocode_outlet_version_id => 1, :text => "Your verification code is #{@customer.verification_code} Thanks, GullakMaster")
                   @sms_sent.send_message
                   @mobile_number = true
               else
