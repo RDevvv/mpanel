a3b0d0d2a3976c96b52febf0a8f71022cdca07bf
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-02-25 19:23:07 +0530

    admin_panel brands#index improved

--- app/admin/brands.rb
+++ app/admin/brands.rb
@@ -1,3 +1,20 @@
 ActiveAdmin.register Brand do
-  
+  menu :label => "Redundant Geocoding"
+
+  index do
+    column :id do |brand|
+      link_to "#{brand.id}"
+    end
+    column :brand_name
+    column :website_url
+    column :is_active
+    column :is_verified
+    actions :defaults => false do |brand|
+      link_to "View", admin_brand_path(brand)
+    end
+    actions :defaults => false do |brand|
+      link_to "update", edit_admin_brand_path(brand)
+    end
+  end
+
 end
