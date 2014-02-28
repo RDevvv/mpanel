875e7b8a84ad3b498db821718efbec698fac9aae
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-02-28 17:57:14 +0530

    users controller functional test improved

--- test/functional/merchant/users_controller_test.rb
+++ test/functional/merchant/users_controller_test.rb
@@ -19,7 +19,7 @@ class Merchant::UsersControllerTest < ActionController::TestCase
   end
 
   test "should create user" do
-      post :create, user: { first_name: @user.first_name, last_name: @user.last_name, mobile_number: @user.mobile_number, user_key: @user.user_key, is_active: @user.is_active, email_verified: @user.email_verified, mobile_verified: @user.mobile_verified, email: @user.email, password: @user.password, password_confirmation: @user.password_confirmation, remember_me: @user.remember_me, subscribe_email_updates: @user.subscribe_email_updates, subscribe_crm_updates: @user.subscribe_crm_updates }
+    post :create, user: { first_name: @user.first_name, last_name: @user.last_name, mobile_number: @user.mobile_number, user_key: @user.user_key, is_active: @user.is_active, email_verified: @user.email_verified, mobile_verified: @user.mobile_verified, email: @user.email, password: @user.password, password_confirmation: @user.password_confirmation, remember_me: @user.remember_me, subscribe_email_updates: @user.subscribe_email_updates, subscribe_crm_updates: @user.subscribe_crm_updates }
   end
 
   test "should get edit" do
@@ -31,7 +31,11 @@ class Merchant::UsersControllerTest < ActionController::TestCase
   end
 
   test "should destroy an user" do
-      delete :destroy, id: @user
+    delete :destroy, id: @user
+  end
+
+  test "should add user" do
+
   end
 
 end
