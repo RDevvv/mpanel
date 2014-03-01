ed61ab3661b20fbfc8292a72c7a7ef42f6a80c2d
vivek varade <varade.vivek.123@gmail.com>
2014-03-01 15:25:50 +0530

    application controller updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,17 +27,11 @@ class ApplicationController < ActionController::Base
         "merchant"
     elsif controller_name == 'home' && action_name == 'index'
         "home_index"
-    elsif controller_name == 'home' && action_name == 'combo_view'
-        "desktop"
     elsif controller_name == 'home' && action_name == 'admin_panel'
         "admin"
     elsif controller_name == 'home'
         "listing"
-    elsif controller_name == 'campaigns'
-        "home_index"
-    elsif controller_name == 'customers'
-        "home_index"
-    elsif controller_name == 'ads'
+    elsif controller_name == 'campaigns' || controller_name == 'customers' || controller_name == 'ads'
         "home_index"
     else
         "application"
