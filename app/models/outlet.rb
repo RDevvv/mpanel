5bb705a2a0875e8aab1591a770e275a5a946ce55
vivek varade <varade.vivek.123@gmail.com>
2014-03-05 12:37:22 +0530

    promocode usage limit being checked

--- app/models/outlet.rb
+++ app/models/outlet.rb
@@ -282,7 +282,7 @@ class Outlet < ActiveRecord::Base
           if outlet.ads.blank?
                   outlets_without_ads += [:ad_id => 0, :outlet_id => outlet.id, :distance => outlet.distance, :area_name => outlet.area.area_name, :city_name => outlet.area.city.city_name, :pincode => outlet.area.pincode, :latitude => outlet.latitude, :longitude => outlet.longitude, :shop_no => outlet.shop_no, :address => outlet.get_address, :mobile_number => outlet.mobile_number, :phone_number => outlet.phone_number, :ad_is_exclusive => false, :brand_name => outlet.account_brand.brand.brand_name, :brand => outlet.account_brand.brand, :ad_usage => -1, :ad_expiry_date => Date.today+100.years]
           else
-              ads = outlet.ads.select{|ad|ad.expired? == false}.select{|ad|ad.check_day.include?(Date.today.wday)}
+              ads = outlet.ads.select{|ad|(ad.expired? == false)&(ad.promocode_available?)}.select{|ad|ad.check_day.include?(Date.today.wday)}
               ads.each do |ad|
                   outlets_with_ads += [:ad_id => ad.id, :outlet_id => outlet.id, :distance => outlet.distance, :area_name => outlet.area.area_name, :city_name => outlet.area.city.city_name, :pincode => outlet.area.pincode, :latitude => outlet.latitude, :longitude => outlet.longitude, :shop_no => outlet.shop_no, :address => outlet.get_address, :mobile_number => outlet.mobile_number, :phone_number => outlet.phone_number, :ad_is_exclusive => ad.is_exclusive, :brand_name => outlet.account_brand.brand.brand_name, :brand => outlet.account_brand.brand, :ad_usage => ad.usage, :ad_expiry_date => ad.expiry_date]
               end
