8c1197b3887924e054be3c806a826e43e106e2c9
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-03-10 17:48:08 +0530

    charts improved

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: codemaster123
+ * Date: 3/10/14
+ * Time: 5:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+//= require jquery
+//= require jquery_ujs
+//= require twitter/bootstrap
+//= require highcharts
+
