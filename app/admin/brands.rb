b619a96bbe43b08fb0b345825f3f27a58ab1cffc
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-03-22 14:38:39 +0530

    brand image update functionality added in adminpanel

--- app/admin/brands.rb
+++ app/admin/brands.rb
@@ -1,5 +1,4 @@
 ActiveAdmin.register Brand do
-  menu false
 
   index do
     column :id do |brand|
@@ -16,5 +15,19 @@ ActiveAdmin.register Brand do
       link_to "update", edit_admin_brand_path(brand)
     end
   end
+
+	form(:html => { :multipart => true }) do |f|
+    		f.inputs do
+       			f.input :brand_name
+			f.input :website_url
+			f.input :category, :as => :select, :collection => Category.all.map {|c| [c.category_name, c.id]}
+        	end
+		f.inputs "Attachments" do
+      			f.has_many :attachments do |s|
+        			s.input :image, :as => :file
+      			end
+    		end
+  		f.actions
+  	end
 
 end
