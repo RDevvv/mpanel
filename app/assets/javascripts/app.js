66f7029d0882661521fc92a62c78d2c99e9648c5
vivek varade <varade.vivek.123@gmail.com>
2014-03-24 14:32:14 +0530

    app.js updated

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,11 +1,24 @@
 var app = angular.module('app', ['ngRoute']);
 app.config(function($routeProvider){
     $routeProvider
-    .when('/outlet_listing', {
-        templateUrl: 'outlet_listing.html'
+    .when('/outlet_view', {
+        templateUrl: '/templates/outlet_listingg.html',
+        controller: 'ListingController'
+    })
+    .when('/map_view',{
+        templateUrl: '/templates/map_view.html',
+        controller: 'ListingController'
     })
     .when('/',{
-        templateUrl: 'index.html'
+        templateUrl: '/templates/home.html'
     })
-    .otherwise({redirectTo: 'index.html'})
+    .otherwise({redirectTo: '/templates/home.html'})
 });
+
+app.controller('ListingController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
+    $http({
+        method: 'GET',
+        url: 'outlet_listing.json',
+        params :{location: $routeParams['location'], view: $routeParams['view'], search: $routeParams['search'] }
+    });
+}])
