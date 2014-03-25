53734ae9f2f0ad13266fc5262575f0ee4d187b71
vivek varade <varade.vivek.123@gmail.com>
2014-03-25 15:23:11 +0530

    angular updated further

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,11 +2,11 @@ var app = angular.module('app', ['ngRoute']);
 app.config(function($routeProvider){
     $routeProvider
     .when('/outlet_view', {
-        templateUrl: '/templates/outlet_listingg.html',
+        templateUrl: '/templates/outletview.html',
         controller: 'ListingController'
     })
     .when('/map_view',{
-        templateUrl: '/templates/map_view.html',
+        templateUrl: '/templates/poster.html',
         controller: 'ListingController'
     })
     .when('/',{
@@ -20,5 +20,19 @@ app.controller('ListingController', ['$scope', '$http', '$routeParams', function
         method: 'GET',
         url: 'outlet_listing.json',
         params :{location: $routeParams['location'], view: $routeParams['view'], search: $routeParams['search'] }
+    })
+        .then(function(response){
+    $scope.posters = response.data;
     });
+
+    $scope.view_ad = function(brand_name, title, sms_text){
+        $.pnotify({ title: brand_name+' - '+title, text: sms_text,
+        closer_hover: false,
+        sticker_hover: false,
+        animate_speed: 'fast',
+        icon: false,
+        addclass: 'stack-topleft',
+        opacity: .9
+    });
+    }
 }])
--- app/views/home/outlet_listing.json.jbuilder
+++ app/views/home/outlet_listing.json.jbuilder
@@ -1,3 +1,3 @@
 json.array! @poster_data do |poster|
-    json.extract! poster, :ad_id, :outlet_id, :distance, :area_name, :city_name, :pincode, :latitude, :longitude, :shop_no, :address, :mobile_number, :phone_number, :ad_is_exclusive, :brand_name, :ad_usage, :ad_expiry_date #:brand
+    json.extract! poster, :is_unlocked, :brand_image, :ad_id, :sms_text, :ad_title, :outlet_id, :distance, :area_name, :city_name, :pincode, :latitude, :longitude, :shop_no, :address, :mobile_number, :phone_number, :ad_is_exclusive, :brand_name, :ad_usage, :ad_expiry_date #:brand
 end
