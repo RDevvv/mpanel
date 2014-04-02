66ea456e8f41e9ed7098b327dd1fb00a810cf920
vivek varade <varade.vivek.123@gmail.com>
2014-04-02 15:59:41 +0530

    templates updated

--- app/assets/javascripts/services/get_outlets.js
+++ app/assets/javascripts/services/get_outlets.js
@@ -1,12 +1,15 @@
 app.factory('getOutlet',['$routeParams', '$http', function($routeParams, $http){
     return{
         fetch_outlets: function(succeedcb){
+            console.log($routeParams['location'])
+            if($routeParams['location']!='undefined'){
             $http({
                 method: 'GET',
                 url: 'outlet_listing.json',
                 params :{location: $routeParams['location'], view: $routeParams['view'], search: $routeParams['search'], filter: $routeParams['filter'] }
             })
             .then(function(response){ succeedcb(response.data)});
+            }
         }
     };
 }])
