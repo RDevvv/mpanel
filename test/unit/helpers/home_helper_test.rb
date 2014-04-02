3e69deb32b9f7ecb37756ce32731ba838fd69c45
vivek varade <varade.vivek.123@gmail.com>
2014-04-02 11:12:51 +0530

    tests updated

--- test/unit/helpers/home_helper_test.rb
+++ test/unit/helpers/home_helper_test.rb
@@ -15,7 +15,7 @@ class HomeHelperTest < ActionView::TestCase
     end
 
     test "should get brand title which is bigger than 7 characters" do
-        assert_equal "Aura Sp..", get_brand_title(Brand.find(3).brand_name)
+        assert_equal "Aura Spa Spa Spa..", get_brand_title(Brand.find(3).brand_name)
     end
 
     test "should generate referer link" do
