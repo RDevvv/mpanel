b21620739aafb455776b6fc9a853d1c0b2024860
vivek varade <varade.vivek.123@gmail.com>
2014-04-03 14:53:38 +0530

    top menu hidden

--- app/assets/javascripts/controllers/home_controller.js
+++ app/assets/javascripts/controllers/home_controller.js
@@ -1,7 +1,20 @@
 app.controller('HomeController', ['$scope', '$http', '$routeParams', '$cookies', '$location', function($scope, $http, $routeParams, $cookies, $location){
     $scope.home_submit = function(location, latitude, longitude){
-        console.log(location);
         $location.url('/outlet_view?search=all&location='+location+'&latitude='+latitude+'&longitude='+longitude+'&view=outlet_listing')
     }
 
+    $scope.show_top_menu = function(){
+        console.log('within show_top_menu');
+            angular.element('#header').removeClass('hide');
+            angular.element('#top_search').removeClass('hide');
+    }
+
+    hide_top_menu = function(){
+        console.log('within hide_top_menu');
+        if($location.url()=='/'){
+            angular.element('#header').addClass('hide');
+            angular.element('#top_search').addClass('hide');
+        }
+    }
+    hide_top_menu();
 }])
