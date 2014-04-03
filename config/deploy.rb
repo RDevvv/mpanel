d373436f7dfca9645f837ebd4d5242c8c4d7e8ff
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-04-03 12:27:54 +0530

    rake resque:work added in capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,3 +38,21 @@ namespace :deploy do
     end
     after :finishing, 'deploy:cleanup'
 end
+
+after "deploy:symlink", "deploy:restart_workers"
+
+
+def run_remote_rake(rake_cmd)
+  rake_args = ENV['RAKE_ARGS'].to_s.split(',')
+  cmd = "cd #{fetch(:latest_release)} && #{fetch(:rake, "rake")} RAILS_ENV=#{fetch(:rails_env, "production")} #{rake_cmd}"
+  cmd += "['#{rake_args.join("','")}']" unless rake_args.empty?
+  run cmd
+  set :rakefile, nil if exists?(:rakefile)
+end
+
+namespace :deploy do
+  desc "Restart Resque Workers"
+  task :restart_workers, :roles => :db do
+    run_remote_rake "resque:restart_workers"
+  end
+end
