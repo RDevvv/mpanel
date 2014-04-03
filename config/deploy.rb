c675b7959cd42872f5a4c8fbe74be5c177d98f79
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-04-03 13:09:18 +0530

    resque:work through capistrano improved

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,12 +39,12 @@ namespace :deploy do
     after :finishing, 'deploy:cleanup'
 end
 
-after "deploy", "deploy:restart_workers"
-
+after "deploy:restart", "deploy:restart_workers"
 
 def run_remote_rake(rake_cmd)
   rake_args = ENV['RAKE_ARGS'].to_s.split(',')
-  cmd = "cd #{fetch(:latest_release)} && #{fetch(:rake, "rake")} RAILS_ENV=#{fetch(:rails_env, "production")} #{rake_cmd}"
+
+  cmd = "cd #{fetch(:latest_release)} && bundle exec #{fetch(:rake, "rake")} RAILS_ENV=#{fetch(:rails_env, "production")} #{rake_cmd}"
   cmd += "['#{rake_args.join("','")}']" unless rake_args.empty?
   run cmd
   set :rakefile, nil if exists?(:rakefile)
@@ -52,7 +52,7 @@ end
 
 namespace :deploy do
   desc "Restart Resque Workers"
-  task :restart_workers do
+  task :restart_workers, :roles => :worker do
     run_remote_rake "resque:restart_workers"
   end
 end
