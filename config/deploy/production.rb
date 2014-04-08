e8f1b0091feb0a9e7da312363bb4bddbcb509030
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-04-08 13:58:16 +0530

    singapure production ip added in production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -17,7 +17,7 @@ set :deploy_via, :remote_cache
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '54.186.13.218', user: 'ubuntu', roles: %w{web app db resque_worker}
+server '54.255.155.152', user: 'ubuntu', roles: %w{web app db resque_worker}
 set :ssh_options, { :forward_agent => true }
 set :use_sudo, false
 set :rails_env,"production"
