f4a3dd8ba7ebf73abb56a3505a6fbfde162de9b8
vivek varade <varade.vivek.123@gmail.com>
2014-04-09 18:29:02 +0530

    tests updated

--- test/functional/campaigns_controller_test.rb
+++ test/functional/campaigns_controller_test.rb
@@ -8,46 +8,4 @@ class CampaignsControllerTest < ActionController::TestCase
     sign_in @user
     @campaign = campaigns(:one)
   end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:campaigns)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create campaign" do
-    assert_difference('Campaign.count') do
-      post :create, campaign: { expires_at: @campaign.expires_at, keyword: @campaign.keyword, marketer: @campaign.marketer, medium: @campaign.medium, post_expiry_forward_url: @campaign.post_expiry_forward_url, pre_expiry_forward_url: @campaign.pre_expiry_forward_url, short_url: SecureRandom.urlsafe_base64(3), source: @campaign.source, campaign_type: @campaign.campaign_type, unique_key: @campaign.unique_key }
-    end
-
-    assert_redirected_to campaign_path(assigns(:campaign))
-  end
-
-  test "should show campaign" do
-    get :show, id: @campaign
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @campaign
-    assert_response :success
-  end
-
-  test "should update campaign" do
-    put :update, id: @campaign, campaign: { expires_at: @campaign.expires_at, keyword: @campaign.keyword, marketer: @campaign.marketer, medium: @campaign.medium, post_expiry_forward_url: @campaign.post_expiry_forward_url, pre_expiry_forward_url: @campaign.pre_expiry_forward_url, short_url: @campaign.short_url, source: @campaign.source, campaign_type: @campaign.campaign_type, unique_key: @campaign.unique_key }
-    assert_redirected_to campaign_path(assigns(:campaign))
-  end
-
-  test "should destroy campaign" do
-    assert_difference('Campaign.count', -1) do
-      delete :destroy, id: @campaign
-    end
-
-    assert_redirected_to campaigns_path
-  end
 end
--- test/functional/sms_sents_controller_test.rb
+++ test/functional/sms_sents_controller_test.rb
@@ -8,15 +8,4 @@ class SmsSentsControllerTest < ActionController::TestCase
     sign_in @user
     @sms_sent = sms_sents(:one)
   end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:sms_sents)
-  end
-
-  test "should show sms_sent" do
-    get :show, id: @sms_sent
-    assert_response :success
-  end
 end
