174fe90e0421449b5d12140eadd6d52e0d8a2d13
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-04-10 19:03:01 +0530

    article improved

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-  attr_accessible :body, :title, :attachments_attributes
+  attr_accessible :body, :title, :attachments_attributes, :blog_category, :page_title, :blog_title, :blog_url, :blog_keywords,:summary_image, :image_caption, :summary_image_caption, :image_alt_text, :image_title, :blog_excerpt, :blog_body, :author, :author_linkedin_url, :social_share_caption
 
   has_many :attachments, :as => :attachable ,:class_name=>'Attachment'
   accepts_nested_attributes_for :attachments ,allow_destroy: true
