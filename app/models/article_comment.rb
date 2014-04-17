cf37881ef676fe73c4587596204d4a025bbc8b4b
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-04-17 10:19:27 +0530

    validation added for article comments

--- app/models/article_comment.rb
+++ app/models/article_comment.rb
@@ -2,4 +2,7 @@ class ArticleComment < ActiveRecord::Base
   attr_accessible :comment, :commentable_id, :commentable_type, :email, :name
 
   belongs_to :commentable, :polymorphic => true
+  validates_presence_of :comment
+  validates_presence_of :email
+  validates_presence_of :name
 end
