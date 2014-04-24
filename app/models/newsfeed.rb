bdd65ee842264f210ffe6e8de6af5eb3369153bb
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-04-24 14:20:18 +0530

    automatically created campaign after create newsfeed

--- app/models/newsfeed.rb
+++ app/models/newsfeed.rb
@@ -3,4 +3,11 @@ class Newsfeed < ActiveRecord::Base
 
   has_many :attachments, :as => :attachable ,:class_name=>'Attachment'
   accepts_nested_attributes_for :attachments ,allow_destroy: true
+
+  after_create :create_campaign
+
+  def create_campaign
+    Campaign.create(:campaign_name => "#{self.id}"+ "-" +"#{self.news_type}"+" "+"#{self.created_at}"+" "+"#{self.news_title}", :source => "Social Media", :medium => "Wall", :placement =>  "User Feed", :marketer => "Shoffr", :campaign_type => "1TM Manual", :purpose => "Acquisition", :target => "Business")
+  end
+
 end
