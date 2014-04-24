95eac86f454efe9f83817f7580f02d075f518841
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-04-24 12:33:39 +0530

    association added in newsfeed and attachments

--- app/models/newsfeed.rb
+++ app/models/newsfeed.rb
@@ -1,3 +1,6 @@
 class Newsfeed < ActiveRecord::Base
-  attr_accessible :image_alt_text, :image_caption, :image_title, :journal_name, :journal_url, :news_excerpt, :news_title, :news_type, :news_url
+  attr_accessible :image_alt_text, :image_caption, :image_title, :journal_name, :journal_url, :news_excerpt, :news_title, :news_type, :news_url, :attachments_attributes
+
+  has_many :attachments, :as => :attachable ,:class_name=>'Attachment'
+  accepts_nested_attributes_for :attachments ,allow_destroy: true
 end
