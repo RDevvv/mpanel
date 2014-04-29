b46528efb666cbd09103a558dd4088560a2464a0
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-04-29 15:16:35 +0530

    brand logo preview displayed on admin panel

--- app/admin/brands.rb
+++ app/admin/brands.rb
@@ -17,15 +17,36 @@ ActiveAdmin.register Brand do
   end
 
 	form(:html => { :multipart => true }) do |f|
-    		f.inputs do
-       			f.input :brand_name
-			f.input :website_url
-			f.input :category, :as => :select, :collection => Category.all.map {|c| [c.category_name, c.id]}
+    	f.inputs do
+        f.input :brand_name
+			  f.input :website_url
+			  f.input :category, :as => :select, :collection => Category.all.map {|c| [c.category_name, c.id]}
        			f.has_many :attachments do |s|
         			s.input :image, :as => :file
       			end
-		end
+		    end
   		f.actions
-  	end
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :brand_name
+      row :category
+      row :website_url
+      row :is_active
+      row :is_verified
+      row :created_at
+      row :updated_at
+      row :attachments do |brand|
+        if brand.attachments.last.image.present?
+          image_tag brand.attachments.last.image
+        else
+          status_tag('No logo uploaded')
+        end
+      end
+    end
+    active_admin_comments
+  end
 
 end
