cf8aebf3f20a0bbfe210f9268d679c5e288e2c23
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-04-30 18:50:28 +0530

    home page assets pipeline removed

--- app/assets/javascripts/blog.js
+++ app/assets/javascripts/blog.js
@@ -1,8 +1,3 @@
 //= require jquery
 //= require twitter/bootstrap
 //= require moment.min
-//= require jquery.stellar.min
-//= require waypoints.min
-//= require jquery.easing.1.3
-//= require scripts
-//= require jquery.fancybox
