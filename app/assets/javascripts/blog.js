bf5879db8a32fbadd6ada0eac0359295ac72b440
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-04-30 17:01:31 +0530

    assets pipeline apply for static home page

--- app/assets/javascripts/blog.js
+++ app/assets/javascripts/blog.js
@@ -1,3 +1,8 @@
 //= require jquery
 //= require twitter/bootstrap
 //= require moment.min
+//= require jquery.stellar.min
+//= require waypoints.min
+//= require jquery.easing.1.3
+//= require scripts
+//= require jquery.fancybox
