a20c9be3d8cd222dadfa339ab61196bc5fdcee88
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-05-02 17:40:37 +0530

    newsletter improved on admin panel

--- app/admin/newsletters.rb
+++ app/admin/newsletters.rb
@@ -1,3 +1,20 @@
 ActiveAdmin.register Newsletter do
-  
+
+  index do
+    column :id do |newsletter|
+      link_to "#{newsletter.id}", admin_newsletter_path(newsletter)
+    end
+    column (:newsletter_body) {|a| CGI.unescapeHTML(a.newsletter_body).html_safe}
+    actions :defaults => true
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row (:newsletter_body) {|a| CGI.unescapeHTML(a.newsletter_body).html_safe}
+      row :created_at
+      row :updated_at
+    end
+  end
+
 end
