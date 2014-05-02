6677ac4326724a3be8f239d686d8001ece001e7c
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-05-02 18:21:23 +0530

    newsletter url final

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -3,7 +3,7 @@ class NewslettersController < ApplicationController
 
   def show
     created_at = params[:created_at]
-    @newsletter = Newsletter.where("created_at >= '#{(created_at).to_time}' and created_at <= '#{(created_at+(1.to_s)).to_time}'").last
+    @newsletter = Newsletter.where("created_at >= '#{(created_at).to_time.beginning_of_day}' and created_at <= '#{(created_at).to_time.end_of_day}'").last
 
     respond_with @newsletter
   end
