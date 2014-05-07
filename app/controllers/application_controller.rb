838468b23ef825c37c138c467cbddf18c38436cb
vivek varade <varade.vivek.123@gmail.com>
2014-05-07 17:37:26 +0530

    CORS added for sms sent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     before_filter :set_headers
 
     def set_headers
-        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Origin'] = 'www.shoffr.com'
         response.headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
         response.headers['Access-Control-Request-Method'] = '*'
         response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
