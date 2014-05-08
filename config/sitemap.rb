2548ac80206186e06f080cdfefec4feafa6046c1
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-05-08 15:36:10 +0530

    button click removed from sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -6,9 +6,9 @@ SitemapGenerator::Sitemap.create do
     Area.all.each do |area|
         add "/deals/outlets?location=#{area.area_name}&search=all&latitude=00&longitude=00&view=outlet_view", :changefreq => 'daily', :priority => 0.9
     end
-    ButtonClick.all.uniq{|bc|bc.current_link}.each do |bc|
-        if bc.length > 10
-            add bc, :changefreq => 'daily', :priority => 0.9
-        end
-    end
+    #ButtonClick.all.uniq{|bc|bc.current_link}.each do |bc|
+    #    if bc.length > 10
+    #        add bc, :changefreq => 'daily', :priority => 0.9
+    #    end
+    #end
 end
