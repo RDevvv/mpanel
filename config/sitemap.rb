da742f3ab0d444dda9c2cc97a11bb864ec9f1086
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-05-08 15:34:33 +0530

    sitemap config improved

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,14 +1,14 @@
 require 'rubygems'
 require 'sitemap_generator'
 
-SitemapGenerator::Sitemap.default_host = 'http://m.gullakmaster.com'
+SitemapGenerator::Sitemap.default_host = 'http://m.shoffr.com'
 SitemapGenerator::Sitemap.create do
     Area.all.each do |area|
-        add "/outlet_listing?location=#{area.area_name}&view=outlet_listing&search=all", :changefreq => 'daily', :priority => 0.9
+        add "/deals/outlets?location=#{area.area_name}&search=all&latitude=00&longitude=00&view=outlet_view", :changefreq => 'daily', :priority => 0.9
     end
     ButtonClick.all.uniq{|bc|bc.current_link}.each do |bc|
         if bc.length > 10
-            add bc, :changefreq => 'daily', priority: => 0.9
+            add bc, :changefreq => 'daily', :priority => 0.9
         end
     end
 end
