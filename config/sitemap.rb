c242f14262f98fff5f94a38e4fd5d0c9eea68619
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-05-08 15:52:21 +0530

    article added in sitemap config

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,14 +1,18 @@
 require 'rubygems'
 require 'sitemap_generator'
 
-SitemapGenerator::Sitemap.default_host = 'http://m.shoffr.com'
+SitemapGenerator::Sitemap.default_host = 'http://www.shoffr.com'
 SitemapGenerator::Sitemap.create do
-    Area.all.each do |area|
-        add "/deals/outlets?location=#{area.area_name}&search=all&latitude=00&longitude=00&view=outlet_view", :changefreq => 'daily', :priority => 0.9
-    end
+    #Area.all.each do |area|
+    #    add "/deals/outlets?location=#{area.area_name}&search=all&latitude=00&longitude=00&view=outlet_view", :changefreq => 'daily', :priority => 0.9
+    #end
     #ButtonClick.all.uniq{|bc|bc.current_link}.each do |bc|
     #    if bc.length > 10
     #        add bc, :changefreq => 'daily', :priority => 0.9
     #    end
     #end
+  Article.all.each do |article|
+    add "/articles/#{article.blog_url}", :changefreq => 'never', :priority => 0.9
+  end
+
 end
