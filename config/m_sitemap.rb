8c6a2ce57b0e0e74fbcb6f5f66868af4ec50c847
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2014-05-09 15:04:17 +0530

    sitemap updated

--- config/m_sitemap.rb
+++ config/m_sitemap.rb
@@ -6,7 +6,12 @@ SitemapGenerator::Sitemap.sitemaps_path = "sitemaps/mobile"
 SitemapGenerator::Sitemap.create do
 
   Area.all.each do |area|
-    add "/deals/outlets?location=#{area.area_name}&search=all&latitude=00&longitude=00&view=outlet_view", :changefreq => 'daily', :priority => 0.9
+    add "/#/deals/outlets?location=#{area.area_name}&search=all&latitude=00&longitude=00&view=outlet_view", :changefreq => 'daily', :priority => 0.9
+    add "/#/deals/map?location=#{area.area_name}&search=all&latitude=00&longitude=00&view=outlet_view", :changefreq => 'daily', :priority => 0.9
   end
+
+ # Outlet.all.each do |outlet| do
+ #   add "/#/deals/shop?id=#{outlet.id}&customer_latitude=19.055229&customer_longitude=72.830829&distance=0.43", :changefreq => 'daily', :priority => 0.9
+ # end
 
 end
