b9cee916cb29d989324fb25b66aa314bd7266bba
vivek varade <varade.vivek.123@gmail.com>
2014-05-15 14:02:16 +0530

    back button icon changed

--- app/assets/javascripts/directives/top_header.js
+++ app/assets/javascripts/directives/top_header.js
@@ -2,12 +2,12 @@ app.directive('topHeader', function($location, $routeParams){
     return{
         restrict: 'E',
         replace: true,
-        template: '<div><a href="javascript:history.back()" class="backpage">&nbsp;</a><a href="#/home">{{header}}</a></div>',
+        template: '<div><a href="javascript:history.back()" class="{{back_link}}">&nbsp;</a><a href="#/home">{{header}}</a></div>',
         link: function(scope){
             scope.$on('$locationChangeSuccess', function(){
                 if($location.path()=='/deals/shop'){
                     scope.header = $location.search().brand_name;
-                    scope.back_link= $(scope.back_link);
+                    scope.back_link= 'backpage';
                 }
                 else{
                     scope.header = 'shoffr';
