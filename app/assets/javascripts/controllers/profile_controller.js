f9fb3fcdf93a94b21e258721b8ae1c24af2b8707
vivek varade <varade.vivek.123@gmail.com>
2014-05-17 09:29:31 +0530

    profile updated

--- app/assets/javascripts/controllers/profile_controller.js
+++ app/assets/javascripts/controllers/profile_controller.js
@@ -10,4 +10,19 @@ app.controller('ProfileController',function($scope, $http, $cookies, UserProfile
         $scope.name = data['name'];
         $scope.mobile_number = data['mobile_number'];
     })
+
+    $scope.profile_submit = function(){
+        console.log('profile_submitted');
+        $http({
+            method: 'POST',
+            url: 'get_profile.json',
+            params: {
+                mobile_number: $scope.mobile_number,
+                customer_uuid: $cookies['customer_uuid']
+            }
+        }).success(function(data){
+            $scope.name = data['name'];
+            $scope.mobile_number = data['mobile_number'];
+        })
+    }
 })
