6dde6a7bad6a8469b83415dc718f8896b7f43029
vivek varade <varade.vivek.123@gmail.com>
2014-05-17 14:07:29 +0530

    profile page added

--- app/assets/javascripts/controllers/profile_controller.js
+++ app/assets/javascripts/controllers/profile_controller.js
@@ -1,4 +1,6 @@
-app.controller('ProfileController',function($scope, $http, $cookies, UserProfile){
+app.controller('ProfileController',function($scope, $http, $cookies, $location, $timeout, UserProfile){
+    $scope.number_submit_message = false;
+    $scope.verification_code_submit_message = false;
     $http({
         method: 'GET',
         url: 'get_profile.json',
@@ -11,18 +13,42 @@ app.controller('ProfileController',function($scope, $http, $cookies, UserProfile
         $scope.mobile_number = data['mobile_number'];
     })
 
-    $scope.profile_submit = function(){
-        console.log('profile_submitted');
-        $http({
-            method: 'POST',
-            url: 'get_profile.json',
-            params: {
-                mobile_number: $scope.mobile_number,
-                customer_uuid: $cookies['customer_uuid']
-            }
-        }).success(function(data){
-            $scope.name = data['name'];
-            $scope.mobile_number = data['mobile_number'];
-        })
+    $scope.check_size = function(){
+        if($scope.mobile_number.length==10){
+            $http({
+                method: 'POST',
+                url: "/get_mobile_number.json",
+                params: {
+                    mobile_number: $scope.mobile_number,
+                    customer_uuid: $cookies['customer_uuid']
+                }
+            }).success(function(data){
+                $scope.phone_number_message = 'vivek';
+                $scope.number_submit_message = true;
+            });
+        }
+    }
+
+    $scope.check_verification_code_size = function(){
+        if($scope.verification_code.length==4){
+            $http({
+                url: "/check_verification_code.json",
+                method: 'POST',
+                params: {
+                    verification_code: $scope.verification_code,
+                    customer_uuid: $cookies['customer_uuid']
+                }
+            }).success(function(data){
+                if(data["verified"]==true){
+                    $scope.verification_message = 'Your number is verified, redirection to home page ...';
+                    $timeout(function(){
+                        $location.path('home')
+                    },2000);
+                }
+                else
+                    $scope.verification_message = 'Please enter a correct verification code';
+            });
+            $scope.verification_code_submit_message = true;
+        }
     }
 })
