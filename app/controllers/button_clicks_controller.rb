1dabcbb3e030212503e64e0aa1f3f406c3e22d41
vivek varade <varade.vivek.123@gmail.com>
2014-05-31 18:14:59 +0530

    button clicks updated

--- app/controllers/button_clicks_controller.rb
+++ app/controllers/button_clicks_controller.rb
@@ -2,14 +2,6 @@ class ButtonClicksController < ApplicationController
     before_filter :set_headers
     skip_before_filter  :verify_authenticity_token, :only => [:get_click]
 
-    def set_headers
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-        response.headers['Access-Control-Request-Method'] = '*'
-        response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-        response.headers['Accept-Encoding'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    end
-
     def get_click
         customer = Customer.where(:uuid => params[:customer_uuid]).first
         customer_session  = CustomerSession.where(:customer_id => customer.id)
