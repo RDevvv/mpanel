2c024ecdac4b266f97249219899537b2a5d0a8ff
vivek varade <varade.vivek.123@gmail.com>
2014-05-31 17:59:15 +0530

    helpers cleaned

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,2 @@
 module ApplicationHelper
-    def get_og_tags
-        uri = request.env["REQUEST_URI"]
-        short_url = request.env["action_dispatch.request.unsigned_session_cookie"]["referer"][1][:path].gsub("/","")
-        campaign_copy = CampaignCopy.where(:short_url => short_url)
-        if campaign_copy.blank?
-            og_title = "GullakMaster | Deals at your Fingertips"
-            og_image = "http://www.gullakmaster.com/assets/gullak-logo-mcc.png"
-        else
-            ad = campaign_copy.first.campaign.ad_promocode_outlet.ad
-            og_title = ad.ad_title+" "+ad.account_brand.brand.brand_name
-
-            if ad.attachments.blank?
-                og_image = "http://www.gullakmaster.com/assets/gullak-logo-mcc.png"
-            else
-                og_image = "http://www.gullakmaster.com/"+ad.attachments.first.image
-            end
-        end
-        @tags = [og_title, og_image]
-    end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,60 +1,4 @@
 module HomeHelper
-    def generate_title
-        if controller_name == 'home'
-            if action_name == 'index'
-                return "GullakMaster - Home"
-            else
-                return "GullakMaster - Deals"
-            end
-        end
-    end
-
-    def generate_referer_link(outlet, referer_uuid, ad)
-        customer_id = Customer.where(:uuid => referer_uuid).first.id
-
-        url = "http://staging.gullakmaster.com/"
-        url += "deals/organic/"
-        url += "facebook/"
-        url += outlet.area.city.city_name.tr(" ","_")+"/"
-        url += outlet.area.area_name.tr(" ", "_")+"/"
-        url += outlet.account_brand.brand.category.category_name.tr(" ","_")+"/"
-        url += ad.ad_promocode_outlets.where(:outlet_id => outlet.id).first.id.to_s+"/"
-
-        return url
-    end
-
-    def get_brand_image(brand)
-        if brand.attachments.blank?
-            image_tag "categories/#{brand.category.category_name.gsub(" ", "_")}.png", :class => "img-responsive"
-        else
-            image_tag brand.attachments.first.image, :class => "img-responsive"
-        end
-    end
-
-    def get_brand_title(brand_name)
-        trailing_string=''
-        unless brand_name.length < 17
-            trailing_string='..'
-        end
-        brand_name[0..15]+trailing_string
-    end
-
-    def get_location_search_url
-        if action_name == 'outlet_listing'
-            'outlet_listing'
-        else
-            'map_listing'
-        end
-    end
-
-    def get_keyword_search_url
-        if action_name == 'outlet_listing'
-            'outlet_search'
-        else
-            'map_search'
-        end
-    end
-
     def check_if_verified(uuid)
         customer =Customer.where(:uuid => uuid)
         if customer.blank?
@@ -63,22 +7,6 @@ module HomeHelper
             return 0
         else 
             1
-        end
-    end
-
-    def get_opposite_view(current_view)
-        if current_view =='outlet_listing'
-            view = 'map_listing'
-        elsif current_view == 'map_listing'
-            view = 'outlet_listing'
-        end
-    end
-
-    def get_opposite_view_class(current_view)
-        if current_view =='outlet_listing'
-            view = 'loc'
-        elsif current_view == 'map_listing'
-            view = 'outlet'
         end
     end
 end
