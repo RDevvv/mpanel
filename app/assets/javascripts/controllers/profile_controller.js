f896d507d964adf3a1fdd28d114998928ab6e0be
vivek varade <varade.vivek.123@gmail.com>
2014-06-07 14:25:03 +0530

    cookie issue fixed

--- app/assets/javascripts/controllers/profile_controller.js
+++ app/assets/javascripts/controllers/profile_controller.js
@@ -47,7 +47,7 @@ app.controller('ProfileController',function($scope, $http, $cookies, $location, 
                     customer_uuid: $cookies.customer_uuid
                 }
             }).success(function(data){
-                $cookies.customer_uuid = data['customer_uuid'];
+                $cookies.customer_uuid = data.customer_uuid;
                 $scope.number_submit_message = true;
             });
         }
