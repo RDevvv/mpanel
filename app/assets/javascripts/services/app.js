9bb03f1450622fe7262ba70127f07cc6cf4dd572
vivek varade <varade.vivek.123@gmail.com>
2014-07-10 19:40:27 +0530

    shopping_list route changed

--- app/assets/javascripts/services/app.js
+++ app/assets/javascripts/services/app.js
@@ -22,7 +22,7 @@ app.config(function($routeProvider){
         controller: 'GeocodingController'
     })
     .when('/shopping_list',{
-        templateUrl: 'templates/shopping_list',
+        templateUrl: 'templates/shopping_list.html',
         controller: 'ShoppingListController'
     })
     .otherwise({
