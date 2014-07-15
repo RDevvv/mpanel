e006b941b5eb4d9c9ef225bf51cc0d434b5c80d4
vivek varade <varade.vivek.123@gmail.com>
2014-07-15 10:57:13 +0530

    debugging info added

--- app/controllers/native_notifications_controller.rb
+++ app/controllers/native_notifications_controller.rb
@@ -26,16 +26,21 @@ class NativeNotificationsController < ApplicationController
         @notifications_sent = @customer.native_notifications.map{|notification|notification.ad_id}
         @final_ads = @final_ads-@notifications_sent
         unless @final_ads.blank?
+            Rails.logger.info "Within unless @final_ads.blank?"
             @notification_ad = Ad.find(@final_ads.first)
             destination = [@customer.gcm_registration_id]
             brand_name = @notification_ad.account_brand.brand.brand_name
             data = {:message => brand_name+' - '+@notification_ad.sms_text, :msgcnt => "1", :soundname => "beep.wav"}
 
             @location_changed_flag = @customer.check_if_location_changed_significantly(params[:latitude], params[:longitude])
+            Rails.logger.info "Timeframe = #{NativeNotification.check_if_within_timeframe}"
+            Rails.logger.info "@location_changed_flag = #{@location_changed_flag}"
 
             if (NativeNotification.check_if_within_timeframe)&&(@location_changed_flag>1)
+                Rails.logger.info "Within if(NativeNotification.check_if_within_timeframe)"
                 @similar_notification = NativeNotification.where(:ad_id => @notification_ad.id, :customer_id => @customer.id)
                 unless(@similar_notification.exists?)
+                    Rails.logger.info "Within unless(@similar_notification.exists?)"
                     GCM.send_notification( destination, data)
                     @native_notification = @customer.native_notifications.create(:ad_id => @notification_ad.id)
                     @customer.customer_locations.last.update_attributes(:native_notification_id => @native_notification.id)
