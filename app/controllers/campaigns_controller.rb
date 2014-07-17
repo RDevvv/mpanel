c8611f552085af47e8867a9629672ead2aba5e63
vivek varade <varade.vivek.123@gmail.com>
2014-07-17 17:13:10 +0530

    campaign landing method removed

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -12,7 +12,7 @@ class CampaignsController < ApplicationController
           elsif(campaign_copy.campaign.expires_at.blank?)||(campaign.expires_at > Date.today-1)
               redirect_to 'http://www.shoffr.com/'+campaign_copy.pre_expiry_forward_url+"&short_url="+campaign_copy.short_url
           else
-              redirect_to 'http://awww.hoffr.com/'+campaign_copy.post_expiry_forward_url+"&short_url="+campaign_copy.short_url
+              redirect_to 'http://www.shoffr.com/'+campaign_copy.post_expiry_forward_url+"&short_url="+campaign_copy.short_url
           end
       end
   end
@@ -22,23 +22,6 @@ class CampaignsController < ApplicationController
 
       respond_to do |format|
           format.json {render :json => {:short_url => campaign_copy.short_url}}
-      end
-  end
-
-  def get_campaign_details
-      customer = Customer.where(:uuid => params[:customer_uuid]).first
-      if params[:button_class] == 'top_share'||params[:button_class] == 'new_top_share'
-          campaign_copy = Campaign.where(:medium => 'Email', :marketer => 'Merchant').first.campaign_copies.create(:customer_id => customer.id)
-          image_path = 'www.gullakmaster.com/assets/categories/Gullak_Master_Top.jpg'
-          render :json => {:short_url => campaign_copy.short_url, :image_url => image_path}
-      else
-          ad = Ad.find(params[:ad_id])
-          outlet = Outlet.find(params[:outlet_id])
-          ad_promocode_outlet_ids = AdPromocodeOutlet.where(:ad_id => ad.id, :outlet_id => outlet.id).map{|apo|apo.id}
-          campaign_copy = Campaign.where(:ad_promocode_outlet_id => ad_promocode_outlet_ids).first.campaign_copies.create(:customer_id => customer.id)
-          category = ad.account_brand.brand.category
-          image_path = "www.gullakmaster.com/assets/categories/"+category.name.gsub(" ","_")+"_big.jpg"
-          render :json => {:brand_name =>ad.account_brand.brand.brand_name, :short_url => campaign_copy.short_url, :image_url => image_path, :category_name => category.name, :ad_title => ad.sms_text}
       end
   end
 
