37e1ca6ea207badec671ca248ecb628906a538d7
vivek varade <varade.vivek.123@gmail.com>
2014-07-24 10:26:54 +0530

    cookies created before all API requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
     layout :set_layout
 
     before_filter :set_headers
+    before_filter :check_cookies, :except => [:check_app_cookies]
+    before_filter :record_session
+    before_filter :get_referer, :only => [:index]
 
     def set_headers
         response.headers['Access-Control-Allow-Origin'] = '*'
@@ -10,6 +13,43 @@ class ApplicationController < ActionController::Base
         response.headers['Access-Control-Request-Method'] = '*'
         response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
         response.headers['Accept-Encoding'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    end
+
+    def check_cookies
+        if cookies[:customer_uuid].blank?
+            cookies[:customer_uuid] = {:value => Customer.generate_cookie, :expires => 1.year.from_now}
+            @customer = Customer.create(:uuid => cookies[:customer_uuid])
+        end
+    end
+
+    def check_app_cookies
+        params[:customer_uuid] = nil if params[:customer_uuid] == 'undefined'
+        if params[:customer_uuid].blank?
+            @customer = Customer.create(:uuid => Customer.generate_cookie)
+        else
+            @customer = Customer.where(:uuid => params[:customer_uuid]).first
+        end
+        return @customer
+    end
+
+    def record_session
+        unless session[:new_session] == 1
+            session[:new_session] = 1
+            agent = request.env['HTTP_USER_AGENT']
+            referer_agent = request.env['HTTP_REFERER']
+            parsed_agent = UserAgent.parse(agent)
+            campaign_url = request.env['HTTP_HOST']+request.env['ORIGINAL_FULLPATH']
+            customer = Customer.includes(:customer_sessions).where(:uuid => cookies[:customer_uuid]).first
+            unless customer.blank?
+                if customer.mobile_number.nil?
+                    cookies[:mobile_number] = {:value => false, :expires => 1.year.from_now}
+                elsif customer.is_verified == true
+                    cookies[:mobile_number] = {:value => 'verified', :expires => 1.year.from_now}
+                end
+                CustomerSession.create(:campaign_url => campaign_url, :referer_link => referer_agent, :customer_id => customer.id, :browser_version => parsed_agent.version, :platform => parsed_agent.platform, :browser => parsed_agent.browser, :customer_ip => request.ip)
+                session[:customer_id] = customer.id
+            end
+        end
     end
 
     def user_for_paper_trail
