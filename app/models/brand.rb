f845b3a06746f5c0794d3c84e95f535cbed16c70
Vivek Varade <varade.vivek.123@gmail.com>
2014-08-20 12:34:27 +0530

    facebook, twitter counts being displayed

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,15 +1,19 @@
 class Brand < ActiveRecord::Base
-  attr_accessible :brand_id, :brand_name, :category_id, :is_active, :is_verified, :website_url,:attachments_attributes
+  has_one :customer_brand_score
   has_many :account_brands, :dependent => :destroy
   has_many :accounts, :through => :account_brands
-  belongs_to :category
-  scope :by_brand_name ,lambda{|name|where("lower(brands.brand_name) like (?)","%#{name}%") if name.present?}
-  has_many :attachments, :as => :attachable ,:class_name=>'Attachment'
-  accepts_nested_attributes_for :attachments ,allow_destroy: true
-  validates :brand_name, :uniqueness => true, :presence => true
   has_many :user_brands, :dependent => :destroy
   has_many :users, :through => :user_brands
-  has_one :customer_brand_score
+  has_many :attachments, :as => :attachable ,:class_name=>'Attachment'
+  belongs_to :category
+
+  attr_accessible :brand_id, :brand_name, :category_id, :is_active, :is_verified, :website_url,:attachments_attributes
+  attr_accessible :twitter_followers_count, :twitter_handle, :facebook_handle, :facebook_likes_count, :facebook_username, :facebook_id
+  accepts_nested_attributes_for :attachments ,allow_destroy: true
+
+  validates :brand_name, :uniqueness => true, :presence => true
+
+  scope :by_brand_name ,lambda{|name|where("lower(brands.brand_name) like (?)","%#{name}%") if name.present?}
 
   def name
     self.brand_name
