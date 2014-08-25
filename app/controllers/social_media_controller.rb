cba8072a84bd1cc24f317d9131797e2c96d3ebe7
Vivek Varade <varade.vivek.123@gmail.com>
2014-08-25 15:51:49 +0530

    facebook verification update

--- app/controllers/social_media_controller.rb
+++ app/controllers/social_media_controller.rb
@@ -1,6 +1,7 @@
 class SocialMediaController < ApplicationController
   def facebook_verification
-    params = Hash.new
-    Koala::Facebook::RealtimeUpdates.meet_challenge(params, 'CAACEdEose0cBAI6o8bDZBbIKxcezo8PcZCk0sZBeMZBXpHxdulVvTPUj3QEAEKCMEgHiT0IF05hychkD7jsjEXQnAQpox3RlqpXnIsYOmcPm7wig7YluqTGdkGCfVpzJ5IVghRrtq35yN7xl9u1pCHsruY3UVo1ZA23SCbMCz2wADI5c7EWUkzVutZBOMGlf9iq3mCVPfIET9e5zextkub')
+    @params = Hash.new
+    @params[:verify_token] = 'asdf'
+    Koala::Facebook::RealtimeUpdates.meet_challenge(@params, 'CAACEdEose0cBAI6o8bDZBbIKxcezo8PcZCk0sZBeMZBXpHxdulVvTPUj3QEAEKCMEgHiT0IF05hychkD7jsjEXQnAQpox3RlqpXnIsYOmcPm7wig7YluqTGdkGCfVpzJ5IVghRrtq35yN7xl9u1pCHsruY3UVo1ZA23SCbMCz2wADI5c7EWUkzVutZBOMGlf9iq3mCVPfIET9e5zextkub')
   end
 end
