2fe6e5eb89a2ee224c83ed0a9e7fb18d4e2d420e
Vivek Varade <varade.vivek.123@gmail.com>
2014-08-25 16:15:24 +0530

    social media update

--- app/controllers/social_media_controller.rb
+++ app/controllers/social_media_controller.rb
@@ -3,5 +3,6 @@ class SocialMediaController < ApplicationController
     @params = Hash.new
     @params[:verify_token] = 'asdf'
     Koala::Facebook::RealtimeUpdates.meet_challenge(@params, 'CAACEdEose0cBAI6o8bDZBbIKxcezo8PcZCk0sZBeMZBXpHxdulVvTPUj3QEAEKCMEgHiT0IF05hychkD7jsjEXQnAQpox3RlqpXnIsYOmcPm7wig7YluqTGdkGCfVpzJ5IVghRrtq35yN7xl9u1pCHsruY3UVo1ZA23SCbMCz2wADI5c7EWUkzVutZBOMGlf9iq3mCVPfIET9e5zextkub')
+    render :text => @params[:challenge]
   end
 end
